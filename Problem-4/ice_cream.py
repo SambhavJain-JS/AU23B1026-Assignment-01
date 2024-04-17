4429da55973158b8388adc5bc22072b3cb984719
SambhavJain-JS <165653608+SambhavJain-JS@users.noreply.github.com>
2024-04-18 01:50:13 +0530

    Create ice_cream.py

---
+++ Problem-4/ice_cream.py
@@ -0,0 +1,13 @@
+def add_topping(scoop_size,*toppings):
+    print('your order is being prepared of ',scoop_size,'with toppings',toppings)
+
+def make_shake(flavour):
+    if flavour=='nuts':
+        print('your order is being prepared of ',flavour,'flavvour shake')
+
+    elif flavour=='fruit':
+        print('your order is being prepared of ',flavour,'flavvour shake')
+
+    else:
+        print('this flaur is not available')
+
