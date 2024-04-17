cfa608055cbd0ec85dc00b005318347e861c1574
SambhavJain-JS <165653608+SambhavJain-JS@users.noreply.github.com>
2024-04-18 01:53:11 +0530

    Create ROI.py

---
+++ Problem-6/ROI.py
@@ -0,0 +1,15 @@
+from financial_term import *
+
+fg_improvement=future_gain_improvement()
+print('Future Gain from Improvement: ',fg_improvement)
+
+tg_improvement=total_gain_improvement()
+print('Total Gain from Improvement: ',tg_improvement)
+
+ag_improvement=annual_gain_improvement()
+print('Annual Gain from Improvement: ',ag_improvement)
+
+print('Annual ROI: ',calculate_ratio(ag_improvement,improvement_cost))
+print('ROI:',calculate_ratio(tg_improvement,improvement_cost))
+
+
